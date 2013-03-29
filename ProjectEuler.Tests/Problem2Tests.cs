8d20c99b75a0883ad7b6fa4e0d3c69fb4f235677
Devin Duden <dlduden@me.com>
2013-03-29 07:24:16 -0400

    Initial dev

---
+++ ProjectEuler.Tests/Problem2Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using ProjectEuler;
+
+namespace ProjectEuler.Tests
+{
+    [TestFixture]
+    public class Problem2Tests
+    {
+        private Problem2 _class;
+
+        [SetUp]
+        public void Setup()
+        {
+            _class = new Problem2();
+        }
+
+        [Test]
+        public void GenerateFibonacciSequence_ForTermsFalseAndInputTen_Zero()
+        {
+            var result = _class.GenerateFibonacciSequence(10);
+            Assert.AreEqual(0, result.Sum());
+        }
+    }
+}
---
+++ ProjectEuler/Problem2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler
+{
+    public class Problem2
+    {
+        public bool EvenValued { get; set; }
+        public bool OddValued { get; set; }
+        public bool ForTerms { get; set; }
+        public bool ForCeiling { get; set; }
+
+        private List<Int32> Sequence = new List<Int32>();
+
+        public List<Int32> GenerateFibonacciSequence(Int32 number)
+        {
+            Sequence.Add(0);
+
+            if (ForTerms && number > 0)
+                GenerateSequenceForTerms(number);
+
+            if (ForCeiling && number > 0)
+                GenerateSequenceForCeiling(number);
+
+            return Sequence;
+        }
+
+        private void GenerateSequenceForTerms(int numberOfTerms)
+        {
+            int previous = 1;
+            int calc = 0;
+            int sum = 0;
+            for (int i = 0; i < numberOfTerms; i++)
+            {
+                if (calc == 0)
+                {
+                    Sequence.Add(previous);
+                    calc = previous + previous;
+                    Sequence.Add(calc);
+                }
+                else
+                {
+                    sum = previous + calc;
+                    Sequence.Add(sum);
+                    previous = calc;
+                    calc = sum;
+                }
+            }
+        }
+
+        private void GenerateSequenceForCeiling(Int32 ceiling)
+        {
+            int previous = 1;
+            int calc = 0;
+            while (Sequence.Sum() < ceiling)
+            {
+                if (calc == 0)
+                {
+                    Sequence.Add(previous);
+                    calc = previous + previous;
+                    Sequence.Add(calc);
+                }
+
+            }
+        }
+    }
+}

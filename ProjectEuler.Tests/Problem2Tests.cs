c09e3886f36a8e7183285c70745c5d0071e0e8b3
Devin Duden <dlduden@me.com>
2013-03-29 07:25:47 -0400

    TC2

--- ProjectEuler.Tests/Problem2Tests.cs
+++ ProjectEuler.Tests/Problem2Tests.cs
@@ -22,5 +22,13 @@ namespace ProjectEuler.Tests
             var result = _class.GenerateFibonacciSequence(10);
             Assert.AreEqual(0, result.Sum());
         }
+
+        [Test]
+        public void GenerateFibonacciSequence_ForTermsTrueAndInputZero_Zero()
+        {
+            _class.ForTerms = true;
+            var result = _class.GenerateFibonacciSequence(0);
+            Assert.AreEqual(0, result.Sum());
+        }
     }
 }
